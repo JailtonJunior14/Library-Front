2ee779cb49934842fe7be4c7d9dcdf18152c5748
JailtonJunior14 <jailtonjrbr@gmail.com>
2026-05-12 20:15:37 -0300

    modais das telas de cadastro.

--- JS/layout.js
+++ JS/layout.js
@@ -23,6 +23,56 @@ export async function loadModalStudents() {
     
 }
 
+export async function loadModalLibrarians() {
+    const response = await fetch('/Components/modalCadastroBibliotecario.html')
+    const html = await response.text();
+
+    const modal = document.getElementById('meuModal');
+    modal.innerHTML = html;
+
+    const btnFechar = modal.querySelector('#btnFechar');
+
+    btnFechar.addEventListener('click', () =>{
+        modal.close();
+    });
+}
+
+
+
+export async function loadModalLoans() {
+    const response = await fetch('/Components/modalCadastroEmprestimo.html')
+    const html = await response.text();
+
+    const modal = document.getElementById('meuModal');
+    modal.innerHTML = html;
+
+    const btnFechar = modal.querySelector('#btnFechar');
+
+    btnFechar.addEventListener('click', () =>{
+        modal.close();
+    });
+}
+
+
+
+export async function loadModalBooks() {
+    const response = await fetch('/Components/modalCadastroLivro.html')
+    const html = await response.text();
+
+    const modal = document.getElementById('meuModal');
+    modal.innerHTML = html;
+
+    const btnFechar = modal.querySelector('#btnFechar');
+
+    btnFechar.addEventListener('click', () =>{
+        modal.close();
+    });
+}
+
+
+
+
+
 export async function loadTableStudents(){
     const response = await fetch('/Components/table.html');
     const html = await response.text();
